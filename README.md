6b53fbd3ba526512d0bb3945951c310ff06fc190
Ygor-Evaldt-dev <evaldtygor@rede.ulbra.br>
2022-10-23 11:33:48 -0300

    adicionado read-me com as dependencias do projeto

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # plataforma-perguntas-e-respostas
+- Plataforma de perguntas e respostas estilo Yahoo respostas.
+
+## Stacks:
+
+### Node.js
+- https://nodejs.org/en/
+
+### Express
+- https://expressjs.com/pt-br/
+
+### Express ejs layouts
+- https://www.npmjs.com/package/express-ejs-layouts
+
+### Body-parser
+- https://www.npmjs.com/package/body-parser
+
+### Ejs
+- https://ejs.co/
+
+### Sequelize
+- https://sequelize.org/docs/v6/getting-started/
+
+### Mysql2
+- https://www.npmjs.com/package/mysql2
+
+### Faker.js
+- https://fakerjs.dev/guide/
--- index.js
+++ index.js
@@ -7,26 +7,16 @@ const port = 80;
 
 // configurations database
 const connection = require('./database/connection');
-connection
-  .authenticate()
-  .then(() => {
-    console.log('Conexão bem sucessida com banco de dados.');
-  })
-  .catch((error) => {
-    console.log(`Falha na conexão com banco de dados: ${error}`);
-  })
 
 // configurations
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
-
-const userRoute = require('./routers/userRouters');
-
 // diretory static arquives
 app.use(express.static('public'));
+// routers
+const userRoute = require('./routers/userRouters');
 
 // get routers
 app.use('/', userRoute);
@@ -34,7 +24,6 @@ app.use('/questions', userRoute);
 app.use('/questions/:id', userRoute);
 app.use('/delete', userRoute);
 app.use('delete/:id', userRoute);
-
 // post routers
 app.use('/save_question', userRoute);
 app.use('/answer', userRoute);
